d79882725658ac9c2d17ab61e893492653db65d8
alec-kr <akramdhan02@gmail.com>
2022-09-18 22:28:09 -0400

    Changed project name

--- FormulaPy/__init__.py
+++
@@ -1,1 +0,0 @@
-from .example import driver_standings, constructor_standings, race_results
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 from distutils.core import setup
 
-setup(name='FormulaPy',
+setup(name='F1PyStats',
       version='1.0',
       description='Python Example Package',
       author='alec-kr',
       author_email='akramdhan02@gmail.com',
-      packages=['FormulaPy'],
+      packages=['F1PyStats'],
      )
