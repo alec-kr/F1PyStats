220cdaccc2cce1fec3c7c20de4c05e20f5801a31
alec-kr <akramdhan02@gmail.com>
2022-09-18 22:19:12 -0400

    Refactored files to suit Python package

---
+++ FormulaPy/__init__.py
@@ -0,0 +1,1 @@
+from .example import driver_standings, constructor_standings, race_results
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='FormulaPy',
+      version='1.0',
+      description='Python Example Package',
+      author='alec-kr',
+      author_email='akramdhan02@gmail.com',
+      packages=['FormulaPy'],
+     )
