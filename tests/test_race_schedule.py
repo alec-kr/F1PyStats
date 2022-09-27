23c9aef1d15c8aef81fa8b307c9e266f64120c14
alec-kr <akramdhan02@gmail.com>
2022-09-26 22:59:57 -0400

    Fixed Pylint errors

--- tests/test_race_schedule.py
+++ tests/test_race_schedule.py
@@ -18,13 +18,13 @@ class TestRaceSchedule:
         '''Test the round numbers returned by get_race_rounds'''
         race_rounds = self.r_sched.get_race_round()
         assert race_rounds == ["1", "2", "3"]
-    
+
     def test_race_names(self):
         '''Test the race names returned by get_race_names'''
         race_names = self.r_sched.get_race_names()
         assert race_names == [
-            "Australian Grand Prix", 
-            "Malaysian Grand Prix", 
+            "Australian Grand Prix",
+            "Malaysian Grand Prix",
             "Bahrain Grand Prix"
         ]
 
@@ -41,20 +41,20 @@ class TestRaceSchedule:
         '''Test the circuit names returned by get_race_circuits'''
         race_circuits = self.r_sched.get_race_circuits()
         assert race_circuits == [
-            "Albert Park Grand Prix Circuit", 
-            "Sepang International Circuit", 
+            "Albert Park Grand Prix Circuit",
+            "Sepang International Circuit",
             "Bahrain International Circuit"
         ]
-    
+
     def test_race_times(self):
         '''Test the race times returned by get_race_schedule_times'''
         race_times = self.r_sched.get_race_schedule_times()
         assert race_times == [
-            "04:30:00Z", 
-            "07:00:00Z", 
+            "04:30:00Z",
+            "07:00:00Z",
             "11:30:00Z"
         ]
-    
+
     def test_race_countries(self):
         '''Test the host countries returned by get_race_countries'''
         race_countries = self.r_sched.get_race_countries()
