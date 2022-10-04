c3a28a82af39e7de36524c491195dd74cdd194bf
Exoutia <jhav365@gmail.com>
2022-10-04 13:03:35 +0530

    A file to contain all files for use of get_driver function

---
+++ f1pystats/driver_info.py
@@ -0,0 +1,21 @@
+"""Contains all the functions used by get_driver()"""
+
+class DriverInfo:
+    def __init__(self, info):
+        self.info = info
+    
+    def get_drivers_names(self):
+        '''Returns the driver's name list'''
+        return [" ".join([i["givenName"], i["familyName"]]) for i in self.info]
+    
+    def get_drivers_dob(self):
+        '''Returns the driver's date of birth list'''
+        return [i["dateOfBirth"] for i in self.info]
+    
+    def get_drivers_nationality(self):
+        """Returns the list of driver's nationality"""
+        return [i["nationality"] for i in self.info]
+    
+    def get_drivers_number(self):
+        """Returns the list of driver's number"""
+        return [i["permanentNumber"] for i in self.info]
