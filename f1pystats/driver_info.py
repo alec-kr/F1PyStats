69af369f69130955ca7a20ede3e5476677c72108
Exoutia <jhav365@gmail.com>
2022-10-04 23:45:47 +0530

    making the function more robust

--- f1pystats/driver_info.py
+++ f1pystats/driver_info.py
@@ -7,17 +7,17 @@ class DriverInfo:
 
     def get_drivers_names(self):
         '''Returns the driver's name list'''
-        return [" ".join([i["givenName"], i["familyName"]]) for i in self.info]
+        return [" ".join([i["givenName"], i["familyName"]]) for i in self.info["MRData"]["DriverTable"]["Drivers"]]
 
     def get_drivers_dob(self):
         '''Returns the driver's date of birth list'''
-        return [i["dateOfBirth"] for i in self.info]
+        return [i["dateOfBirth"] for i in self.info["MRData"]["DriverTable"]["Drivers"]]
 
     def get_drivers_nationality(self):
         """Returns the list of driver's nationality"""
-        return [i["nationality"] for i in self.info]
+        return [i["nationality"] for i in self.info["MRData"]["DriverTable"]["Drivers"]]
 
     def get_drivers_number(self):
         """Returns the list of driver's number"""
-        return [i["permanentNumber"] for i in self.info]
+        return [i["permanentNumber"] for i in self.info["MRData"]["DriverTable"]["Drivers"]]
     
