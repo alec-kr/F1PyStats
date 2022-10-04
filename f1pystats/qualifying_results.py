633fbef40bc1a073b520e3e8b6dbef5e681fba6f
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-10-05 04:55:05 +0530

    Create qualifying_results.py

---
+++ f1pystats/qualifying_results.py
@@ -0,0 +1,21 @@
+"""Contains all functions used by qualifying_results()"""
+class QualifyingResults:
+    def __init__(self,results):
+        self.results=results
+    def get_positions(self):
+        """Returns a list of driver positions"""
+        return [i["position"] for i in self.results]
+    def get_names(self):
+        """Returns a list of driver names"""
+        return [
+            " ".join([i["Driver"]["givenName"], i["Driver"]["familyName"]]) for i in self.results
+        ]        
+    def get_driver_numbers(self):
+        """Returns a list of driver numbers"""
+        return [i["number"] for i in self.results] 
+    def get_constructors(self):
+        """Returns the list of name of the constructors"""
+        return [i["Constructor"]["name"] for i in self.results]
+    def get_qualifying_times(self):
+        """Returns a list of tuple of Q1,Q2,Q3 timings"""
+        return [(i["Q1"],i["Q2"],i["Q3"]) for i in self.results]
