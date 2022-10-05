a39f4b8fe758d02c0957f93a2abfb8a987b78842
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-10-05 23:10:24 +0530

    Update qualifying_results.py

--- f1pystats/qualifying_results.py
+++ f1pystats/qualifying_results.py
@@ -1,5 +1,6 @@
 """Contains all functions used by qualifying_results()"""
 class QualifyingResults:
+    """Class which contains the methods which provide the details qualifying_results()"""
     def __init__(self,results):
         self.results=results
     def get_positions(self):
@@ -7,12 +8,10 @@ class QualifyingResults:
         return [i["position"] for i in self.results]
     def get_names(self):
         """Returns a list of driver names"""
-        return [
-            " ".join([i["Driver"]["givenName"], i["Driver"]["familyName"]]) for i in self.results
-        ]        
+        return [" ".join([i["Driver"]["givenName"], i["Driver"]["familyName"]]) for i in self.results]
     def get_driver_numbers(self):
         """Returns a list of driver numbers"""
-        return [i["number"] for i in self.results] 
+        return [i["number"] for i in self.results]
     def get_constructors(self):
         """Returns the list of name of the constructors"""
         return [i["Constructor"]["name"] for i in self.results]
