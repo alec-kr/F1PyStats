ebcb8399e040e6ac2d45fcb96bfdc2eac226794f
István Bodnár <mail.bodnaristvan@gmail.com>
2022-10-14 02:58:15 +0200

    Introduce flake8 style guide checker (#73)

--- f1pystats/__init__.py
+++ f1pystats/__init__.py
@@ -3,7 +3,7 @@
 __version__ = '0.1.0'
 
 
-from .f1pystats import (driver_standings,
+from .f1pystats import (driver_standings,  # noqa: F401
                         constructor_standings,
                         race_winners,
                         race_table,
