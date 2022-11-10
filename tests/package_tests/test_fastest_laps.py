aced3f226cbf9a869c553b2c85e6c04f8b7abf09
youpong <43817652+youpong@users.noreply.github.com>
2022-11-10 15:45:22 +0900

    merge malay5 (#5)
    * Updated Readme.md to show valuable contributors
    
    * Updating font case
    
    * Added functionality
    
    * Added snakecase format
    
    * Visual changes
    
    * Added new line
    
    * Visual changes
    
    * Visual changes
    
    * Visual changes
    
    * Added snakecase format
    
    * Visual changes
    
    * Removing print statement, which was used for testing
    
    * Added Test case
    
    * Added spaces after ,
    
    Co-authored-by: Malay Damani <damanimalay@gmail.com>
    Co-authored-by: Malay Damani <64472685+malay5@users.noreply.github.com>

---
+++ tests/package_tests/test_fastest_laps.py
@@ -0,0 +1,16 @@
+"""This module contains tests for the fastest_laps() function"""
+
+from .base_test_class import BaseTestClass
+
+
+class TestFastestLaps(BaseTestClass):
+    """Tests for the fastest_laps() function"""
+    def test_fastest_laps(self):
+        """Tests the fastest lap returned from fastest_laps()"""
+        fastest = self.fp.fastest_laps(2010, 1)
+        assert self.get_vals(fastest) == self.get_data("fastest_lap_2010_round_1.json")
+
+    def test_fastest_laps_bad_year(self):
+        """Tests for ValueError if bad year is given to fastest_laps()"""
+        with self.pytest.raises(ValueError):
+            self.fp.fastest_laps(1949, 1)
