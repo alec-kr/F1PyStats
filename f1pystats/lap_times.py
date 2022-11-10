7376ac22f1799a87c4635d90933ba19b721ea0b3
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-11-11 04:09:11 +0900

    Introduce LapTimes#get_fastest_lap

--- f1pystats/lap_times.py
+++ f1pystats/lap_times.py
@@ -18,3 +18,35 @@ class LapTimes:
     def get_lap_time(self):
         """Returns a list of the lap times"""
         return [i["time"] for i in self.results]
+
+    def get_fastest_lap(self):
+        """Returns a fastest lap"""
+        print(self.results[0]["time"])
+        ftime = LapTimes._get_sec(self.results[0]["time"])
+        flap = self.results[0]
+        for i in self.results:
+            time = LapTimes._get_sec(i["time"])
+            if time < ftime:
+                ftime = time
+                flap = i
+        return (flap["position"], flap["driverId"], flap["time"])        
+
+    def _get_sec(time_str):
+        """Get seconds from time string"""
+        s = 0
+        num_str = ''
+        for c in time_str:
+            if '0' <= c and c <= '9':
+                num_str += c
+            elif c == ':':
+                s += int(num_str)
+                s *= 60
+                num_str = ''
+            elif c == '.':
+                s += int(num_str)
+                num_str = '.'
+        if num_str[0] == '.':
+            s += float(num_str)
+        else:
+            s += int(num_str)
+        return s
--- tests/module_tests/test_lap_times.py
+++ tests/module_tests/test_lap_times.py
@@ -28,3 +28,18 @@ class TestLapTimes:
         """Test the lap times returned from get_lap_time"""
         lap_times = self.l_times.get_lap_time()
         assert lap_times == ["1:36.446", "1:37.391", "1:38.458"]
+
+    def test_fastest_lap(self):
+        """Test the fastest lap returned from get_fastest_lap"""
+        fastest_lap = self.l_times.get_fastest_lap()
+        assert fastest_lap == ( "1", "massa", "1:36.446" )
+
+    # Niki Lauda       0:58.79  1974R9 Qualifying
+    # ???           1:23:45     ????R? Race
+    def test_get_sec(self):
+        """Test seconds returned from _get_sec"""
+        assert LapTimes._get_sec(     "58.79") == 58.79
+        assert LapTimes._get_sec(   "0:58.79") == 58.79
+        assert LapTimes._get_sec("1:23:45")    == 5025
+        # Not support for an decimal point at the end.
+        #assert LapTimes._get_sec("1:23:45.")  == 5025
