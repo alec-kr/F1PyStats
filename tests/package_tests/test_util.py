9d32b08d0472c89bf669cb412680232d6cad1647
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-11-14 21:35:00 +0900

    Delete garbage that followed by copy and paste.

--- tests/package_tests/test_util.py
+++ tests/package_tests/test_util.py
@@ -13,13 +13,3 @@ class TestUtil(BaseTestClass):
         assert self.fp._get_sec("1:23:45.67") == 5025.67 # Full format
         assert self.fp._get_sec("")           == 0       # non input        
         assert self.fp._get_sec(".")          == 0       # naked decimal point
-
-    def test_sprint_results(self):
-        """Tests the results returned from sprint_results()"""
-        results = self.fp.sprint_results(2021, 10)
-        assert self.get_vals(results) == self.get_data("sprint_2021_10.json")
-
-    def test_bad_sprint_value(self):
-        """Tests the sprint_results() function if bad values are given"""
-        with self.pytest.raises(ValueError):
-            self.fp.sprint_results(2021, 9)
