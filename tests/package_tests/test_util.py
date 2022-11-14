0d6d6a4287d4fffbb26fa8fda91f21a248939886
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-11-14 21:45:28 +0900

    Making it more readable.

--- tests/package_tests/test_util.py
+++ tests/package_tests/test_util.py
@@ -8,8 +8,9 @@ class TestUtil(BaseTestClass):
 
     def test_get_sec(self):
         """Test seconds returned from _get_sec"""
-        assert self.fp._get_sec(     "58.79") == 58.79   # Niki Lauda 1974 R9 Q
-        assert self.fp._get_sec(      "1"   ) == 1       # Minimum format
-        assert self.fp._get_sec("1:23:45.67") == 5025.67 # Full format
-        assert self.fp._get_sec("")           == 0       # non input        
-        assert self.fp._get_sec(".")          == 0       # naked decimal point
+        assert self.fp._get_sec(     "58.79")  == 58.79  # Niki Lauda 1974 R9 Q
+        assert self.fp._get_sec(      "1"   )  == 1      # Minimum format
+        want = (1 * 3600) + (23 * 60) + 45.678
+        assert self.fp._get_sec("1:23:45.678") == want   # Full format
+        assert self.fp._get_sec("")            == 0      # non input        
+        assert self.fp._get_sec(".")           == 0      # naked decimal point
