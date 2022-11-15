7db21b020da93089e87a2101073cc7bab2677195
youpong <43817652+youpong@users.noreply.github.com>
2022-11-15 12:26:12 +0900

    Fix #94 (#95)
    * Introduce f1pystats._get_sec()
    
    * Implements without int()/float()
    
    * pick smallest value in seconds
    
    * Delete garbage that followed by copy and paste.
    
    * Making it more readable.
    
    * linting1
    
    * linting2
    
    * linting3
    
    * linting4
    
    * linting5
    
    * linting6
    
    * linting7

---
+++ tests/package_tests/test_util.py
@@ -0,0 +1,16 @@
+"""This module contains tests for the utility functions"""
+
+from .base_test_class import BaseTestClass
+
+
+class TestUtil(BaseTestClass):
+    """Tests for the utility functions"""
+
+    def test_get_sec(self):
+        """Test seconds returned from _get_sec"""
+        assert self.fp.get_sec("58.79") == 58.79       # Niki Lauda 1974 R9 Q
+        assert self.fp.get_sec("1") == 1               # Minimum format
+        want = (1 * 3600) + (23 * 60) + 45.678
+        assert self.fp.get_sec("1:23:45.678") == want  # Full format
+        assert self.fp.get_sec("") == 0                # non input
+        assert self.fp.get_sec(".") == 0               # naked decimal point
