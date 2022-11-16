b2bd1545b43d585a9b4cc35618600dd4dabef648
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-11-17 04:20:32 +0900

    Adjust test data.

--- tests/package_tests/test_fastest_laps.py
+++ tests/package_tests/test_fastest_laps.py
@@ -7,10 +7,10 @@ class TestFastestLaps(BaseTestClass):
     """Tests for the fastest_laps() function"""
     def test_fastest_laps(self):
         """Tests the fastest lap returned from fastest_laps()"""
-        fastest = self.fp.fastest_laps(2010, 1)
-        assert self.get_vals(fastest) == self.get_data("fastest_lap_2010_round_1.json")
+        fastest = self.fp.fastest_laps(2022, 20)
+        assert self.get_vals(fastest) == self.get_data("fastest_lap_2022_round_20.json")
 
     def test_fastest_laps_bad_year(self):
         """Tests for ValueError if bad year is given to fastest_laps()"""
         with self.pytest.raises(ValueError):
-            self.fp.fastest_laps(1949, 1)
+            self.fp.fastest_laps(2003, 16)
