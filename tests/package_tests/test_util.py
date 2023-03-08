f8be77036fe2a274c9d4ad18263a45a467b03549
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-03-08 15:00:26 +0900

    Change function name

--- tests/package_tests/test_util.py
+++ tests/package_tests/test_util.py
@@ -6,11 +6,11 @@ from .base_test_class import BaseTestClass
 class TestUtil(BaseTestClass):
     """Tests for the utility functions"""
 
-    def test_get_sec(self):
-        """Test seconds returned from _get_sec"""
-        assert self.fp.get_sec("58.79") == 58.79       # Niki Lauda 1974 R9 Q
-        assert self.fp.get_sec("1") == 1               # Minimum format
+    def test_conv_sec(self):
+        """Test seconds returned from _conv_sec"""
+        assert self.fp._conv_sec("58.79") == 58.79       # N.Lauda 1974 R9 Q
+        assert self.fp._conv_sec("1") == 1               # Minimum format
         want = (1 * 3600) + (23 * 60) + 45.678
-        assert self.fp.get_sec("1:23:45.678") == want  # Full format
-        assert self.fp.get_sec("") == 0                # non input
-        assert self.fp.get_sec(".") == 0               # naked decimal point
+        assert self.fp._conv_sec("1:23:45.678") == want  # Full format
+        assert self.fp._conv_sec("") == 0                # non input
+        assert self.fp._conv_sec(".") == 0               # naked decimal point
