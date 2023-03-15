aafe65dcd6d8b9ed72fbf53e8a5c0dbb88897f7e
youpong <43817652+youpong@users.noreply.github.com>
2023-03-15 11:08:12 +0900

    Introduce a workflow to deploy a site to GitHub Pages (#121)
    * Introduce a workflow to deploy a site to GitHub Pages
    
    * Fix path of f1pystats module
    
    * Specify package explicitly in pyproject.toml
    
    * change sphinx theme to 'furo'

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,6 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import sys, os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'f1pystats'))
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
@@ -32,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 #html_static_path = ['_static']
