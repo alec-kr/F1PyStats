ec667abb7d61e5f772e224dccda0eafdf09f6372
youpong <43817652+youpong@users.noreply.github.com>
2023-04-14 11:52:13 +0900

    Fix duplicated cache key (#134)
    * Set cache key properly
    
    * Fall back to furo
    
    * simple copyright

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ metadata = toml.load(os.path.join(os.path.dirname(__file__), '..', 'pyproject.to
 
 project = "F1PyStats"
 author = ",".join(metadata["authors"])
-copyright = f"2020, 2022, 2023, {author}"
+copyright = f"2023 {author}"
 release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
@@ -35,5 +35,5 @@ exclude_patterns = [] # type: List[str]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'cloud'
+html_theme = 'furo'
 #html_static_path = ['_static']
